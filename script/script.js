049a5f6ecc698cb683dad41b1e4599249e2f5f5e
DanieleTuttolani <danieletuttolani2@gmail.com>
2022-12-06 15:52:01 +0100

    prima milestone

--- script/script.js
+++ script/script.js
@@ -15,14 +15,17 @@ Btn.addEventListener("click" , function (){
     //prezzo definito di default
     const CaKm = parseInt(UKm.value);
     let price = CaKm * 0.21;
-    //variabili di sconto
-
-    const CaUa =parseInt(UAge.value);
     
+    //variabili di sconto
+    const CaUa =parseInt(UAge.value);
     if (CaUa <= 18) {
         price *= 0.8; 
     }else if(CaUa >=65){
         price *= 0.6;
         console.log(price);
     }
+
+    //stampo 
+    let PriceMessage = "il tuo biglietto generato, costo: ";
+    Print.innerText = PriceMessage + price
 });
