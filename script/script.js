3d08179197c3fb3ebbdde5c2eb2ac9daa56e33c0
DanieleTuttolani <danieletuttolani2@gmail.com>
2022-12-06 17:07:52 +0100

    seconda milestone senza bonus

--- script/script.js
+++ script/script.js
@@ -5,12 +5,15 @@ console.log("js ok")
 const UKm = document.getElementById("km");
 const UAge = document.getElementById("age");
 const Btn = document.getElementById("submit");
-const Print = document.getElementById("target");
+const Uname = document.getElementById("name");
+const targetPrice = document.getElementById("targetPrice");
+const targetName = document.getElementById("targetName");
 
 
 
 
-//prova 
+
+//prima milestone
 Btn.addEventListener("click" , function (){
     //prezzo definito di default
     const CaKm = parseInt(UKm.value);
@@ -25,7 +28,8 @@ Btn.addEventListener("click" , function (){
         console.log(price);
     }
 
-    //stampo 
-    let PriceMessage = "il tuo biglietto generato, costo: ";
-    Print.innerText = PriceMessage + price
+    //stampo
+    targetPrice.innerText = price.toFixed(2)
+    targetName.innerText = Uname.value
 });
+//seconda milestone
