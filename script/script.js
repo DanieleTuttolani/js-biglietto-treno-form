128b598d1dd90984da6d11e7d562955183578074
DanieleTuttolani <danieletuttolani2@gmail.com>
2022-12-06 15:39:20 +0100

    prezzi e sconti

---
+++ script/script.js
@@ -0,0 +1,28 @@
+console.log("js ok")
+
+//prendo i target dal documento
+
+const UKm = document.getElementById("km");
+const UAge = document.getElementById("age");
+const Btn = document.getElementById("submit");
+const Print = document.getElementById("target");
+
+
+
+
+//prova 
+Btn.addEventListener("click" , function (){
+    //prezzo definito di default
+    const CaKm = parseInt(UKm.value);
+    let price = CaKm * 0.21;
+    //variabili di sconto
+
+    const CaUa =parseInt(UAge.value);
+    
+    if (CaUa <= 18) {
+        price *= 0.8; 
+    }else if(CaUa >=65){
+        price *= 0.6;
+        console.log(price);
+    }
+});
